7a3796862f98eef4f52c5917e2a0e9dec2a59e3a
Oscar-DL <77495758+Oscar-DL@users.noreply.github.com>
2021-03-11 22:18:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let prinsipalubication = window.pageYOffset;
+window.onscroll = function() {
+    let scrollubication = window.pageYOffset;
+    if(prinsipalubication >= scrollubication ){
+        document.getElementById('navbar').style.top = "0px";
+    }
+    else{
+        document.getElementById('navbar').style.top = "-100px";
+    }
+    prinsipalubication = scrollubication;
+}
+window.onload = function(){
+    var cotainer = document.getElementById('loadercontainer');
+    cotainer.style.visibility = 'hidden';
+    cotainer.style.opacity = '0';
+    var all = document.getElementById('content');
+    all.style.display = 'block';
+}
